12ace8c196322449599118ea1fd24812ef815d9a
mohana gowthami gajula <mohanagajula579@gmail.com>
2021-06-04 14:33:28 +0530

    resloved conflicts from master branch

--- layouts/index.ts
+++ layouts/index.ts
@@ -1,8 +1,4 @@
 export { default as DefaultLayout } from "layouts/DefaultLayout";
-<<<<<<< HEAD
-
-=======
->>>>>>> d787a14d7de9a77705bd620e44a4bc667fefea36
 export { default as AdminLayout } from "./AdminLayout";
 export { default as DoctorDashboardLayout } from "./DoctorDashboardLayout";
 export { default as AuthPageLayout } from "./AuthPageLayout";
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,35 +4,6 @@ import React, { FC } from "react";
 import { Image, Form } from "react-bootstrap";
 // forms
 import Signin from "@components/forms/signin";
-<<<<<<< HEAD
-
-const Home: FC = () => {
-  return (
-    <>
-      <div className="d-flex justify-content-between align-items-center p-3 bg-white">
-        <div>
-          <Image src="/logo.svg" width="100" />
-        </div>
-        <div className="d-flex align-items-center">
-          <h6 className="text-base-black fw-bold me-3">Login </h6>
-          <h6 className="text-base-black fw-bold">Contact Us</h6>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col d-flex justify-content-center align-items-center">
-          <Image src="/home.png" className="img-fluid w-75" />
-        </div>
-        <div className="col h-100 bg-light-primary d-flex flex-column align-items-center py-5 ">
-          <div>
-            <h5 className="text-base-black fw-bold me-auto">Register to Iatros</h5>
-          </div>
-          <div className="w-75 d-flex flex-column">
-            <Signin />
-          </div>
-        </div>
-      </div>
-    </>
-=======
 // layouts
 import { AuthPageLayout } from "layouts";
 
@@ -41,6 +12,7 @@ const Home: FC = () => {
     <AuthPageLayout>
       <div className="border" style={{ height: "95vh" }}>
         <div className="h-100">
+          <p>gowthami</p>
           <div className="row justify-content-center h-100">
             <div className="col-md-6 text-center align-self-center">
               <Image className="img-fluid" src="/home.png" width="500" />
@@ -53,7 +25,6 @@ const Home: FC = () => {
         </div>
       </div>
     </AuthPageLayout>
->>>>>>> d787a14d7de9a77705bd620e44a4bc667fefea36
   );
 };
 export default Home;
