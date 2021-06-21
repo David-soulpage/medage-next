a322a6712db78d7e958f96935be4eb84de84831e
David Nirmal <david.prafull@soulpageit.com>
2021-06-21 17:05:08 +0530

    Receptionist Dashboard Prototype

--- pages/nurse/[id]/dashboard.tsx
+++ pages/nurse/[id]/dashboard.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 // next
 import Head from "next/head";
+import Link from "next/link";
 // layouts
 import NurseDashboardLayout from "layouts/NurseDashboardLayout";
 // components
@@ -65,9 +66,11 @@ const Dashboard = () => {
                     <h6 className="fw-bold m-0">Patient List</h6>
                   </div>
                   <div className="ms-3">
-                    <button className="btn btn-sm btn-outline-primary">
-                      <small>Add new patient</small>{" "}
-                    </button>
+                    <Link href="patient-information">
+                      <button className="btn btn-sm btn-outline-primary">
+                        <small>Add new patient</small>{" "}
+                      </button>
+                    </Link>
                   </div>
                   <div className="ms-auto">
                     <InputGroup className="d-flex align-items-center bg-light">
